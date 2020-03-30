68a4539b1ba9e6e34f54a56d17cf8089143c5e74
Alipudina <alipudina333@gmail.com>
2020-03-30 16:00:05 +0200

    mediaquery is added

--- index.js
+++ index.js
@@ -2,14 +2,25 @@ let questionsContainer= document.querySelector('.questions-container');
 let btn= document.querySelector('.form__btn');
 let submitQuestions= document.querySelector('.submitQuestionsButton');
 let submitQuestionsContainer= document.querySelector('.submitQuestionsButton-container');
+let submitQuestionsNote= document.querySelector('.submitQuestionsNote');
+let newPractice= document.querySelector('.newPractice');
+let formMistake= document.querySelector('.formMistake');
+let form= document.querySelector('.form');
+let formMistakeButton= document.querySelector('.formMistake__button');
+let checkAnswersPopup= document.querySelector('.checkAnswersPopup');
+let checkAnswersPopupButton= document.querySelector('.checkAnswersPopup__button');
+let newPracticeLink= document.querySelector('.newPractice__link');
 
 let mainRightAnswerArray=[];
+let formMistakeButtonNotClicked= true;
+let checkAnswersPopupButtonNotClicked= true;
 
 btn.addEventListener('click', ev => {
   ev.preventDefault();
   let name= document.querySelector('.first-name').value;
   let quantity= document.querySelector('.questions').value;
   let rightAnswerArray=[];
+  submitQuestionsNote.style.display= 'none';
 
   if (name.length>2 && quantity) {
     questionsContainer.innerHTML= `<h3 class="form__heading">Hello ${name}</h3>`;
@@ -26,32 +37,79 @@ btn.addEventListener('click', ev => {
       questionsContainer.innerHTML+= question;
     }
 
+    document.querySelector('.first-name').value='';
+    document.querySelector('.questions').value='';
+    submitQuestionsContainer.style.display= 'block';
+    mainRightAnswerArray=rightAnswerArray
+    rightAnswerArray=[];
+    window.location.href='#questions-container';
 
   } else {
-    let error= `<li>Please fill the form first</b></li>`;
-    questionsContainer.innerHTML= error;
-  }
+    if (formMistakeButtonNotClicked) {
+      formMistake.style.transform= 'translateY(0)';
+      form.style.opacity= '.1';
+    }
 
-  document.querySelector('.first-name').value='';
-  document.querySelector('.questions').value='';
-  submitQuestionsContainer.style.display= 'block';
-  mainRightAnswerArray=rightAnswerArray
-  rightAnswerArray=[];
+  }
 })
 
 // submitQuestionsButton
   submitQuestions.addEventListener('click', ev => {
     let studentInputArray= document.querySelectorAll('.answer');
+    newPractice.style.display= 'block';
+    window.location.href='#newPractice';
 
+
+    if (checkAnswersPopupButtonNotClicked) {
+      questionsContainer.style.opacity= '.1';
+      submitQuestionsContainer.style.opacity= '.1';
+      newPracticeLink.style.opacity= '0';
+        checkAnswersPopup.style.opacity= '1';
+        checkAnswersPopup.style.transform= 'translateX(0)';
+    }
+
+
+      let allQuestions= studentInputArray.length;
+      let studentNote= 0;
     for (let i=0; i<studentInputArray.length; i++) {
 
+
       if(studentInputArray[i].value==mainRightAnswerArray[i]) {
-        console.log('ok');
+        studentNote++;
         studentInputArray[i].style.backgroundColor='green';
       } else {
         console.log('no');
         studentInputArray[i].style.backgroundColor='red';
       }
-    }
 
+    }
+    let noteText= `Your Note is <b>${studentNote}</b> from <b>${allQuestions}</b>`;
+    submitQuestionsNote.innerHTML= noteText;
+    submitQuestionsNote.style.display= 'block';
+  })
+
+  // new practice
+  newPractice.addEventListener('click', ev => {
+    newPractice.style.display= 'none';
+  })
+
+  // formMistakeButton
+  formMistakeButton.addEventListener('click', ev => {
+    formMistake.style.transform= 'translateY(-100%)';
+    form.style.opacity= '1';
+    // formMistakeButtonNotClicked= false;
+  })
+
+  // checkAnswersPopupButton
+  checkAnswersPopupButton.addEventListener('click', ev => {
+  
+    newPracticeLink.style.opacity= '1';
+    newPracticeLink.classList.add('bounceInRight', 'delay-1s');
+
+
+    checkAnswersPopup.style.opacity= '0';
+    checkAnswersPopup.style.transform= 'translateX(-100%)';
+    questionsContainer.style.opacity= '1';
+    submitQuestionsContainer.style.opacity= '1';
+    checkAnswersPopupButtonNotClicked= false;
   })
